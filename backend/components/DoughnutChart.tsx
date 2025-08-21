c8b22f246a4e7f9a648b13a1fa6de08196ef010a
d-rex237 <jmc45855@gmail.com>
2025-08-21 08:25:48 +0100

    changes made to entire projetc

---
+++ backend/components/DoughnutChart.tsx
@@ -0,0 +1,36 @@
+"use client";
+import chart from "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
+Chart.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const data = {
+    datasets: [
+      {
+        label: "Banks",
+        data: [12300, 1231, 5400],
+        backgroundColor: ["#0747b6", "#2265d8", "#2f91fa"],
+      },
+    ],
+    labels: ["Bank 1", "Bank 2", "Bank 3"],
+  };
+  return (
+    <Doughnut
+      data={data}
+      options={{
+        responsive: true,
+        maintainAspectRatio: false,
+
+        cutout: "60%",
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      }}
+    />
+  );
+};
+
+export default DoughnutChart;
